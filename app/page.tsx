c3fab0623a2621b0320dd71f0cde1c3d19cf397e
Alesa <alesa@Alesas-MacBook-Pro.local>
2023-11-30 17:16:03 +0100

    changed the front page test

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,12 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-      <div className="max-w-xl p-8 bg-white shadow-lg rounded-lg text-gray-300 md:max-w-xl">
-        <h1 className="text-3xl font-bold mb-2 font-roboto text-center md:text-left">
+    <div className="">
+      <div className="max-w-xl p-8 bg-white shadow-lg rounded-lg text-gray-300">
+        <h1 className="text-3xl font-bold mb-4 font-roboto">
           Welcome to our digital documents platform
         </h1>
-        <p className="text-gray-700 text-m font-roboto text-center md:text-left">
+        <p className="text-gray-700 text-m font-roboto">
           Where efficiency meets creativity. Our mission is to provide you with
           a seamless experience in creating and securely storing digital
           documents. Whether you're a student, professional, or simply
@@ -14,13 +15,10 @@ export default function Home() {
           effortlessly crafting, organizing, and safeguarding your valuable
           content.
         </p>
-        <div className="text-center md:text-left">
-          <button className="bg-rose-300 text-white font-semibold py-2 px-4 mt-4 rounded hover:bg-rose-00 mx-auto">
-            <Link href="/add-document">Get Started</Link>
-          </button>
-        </div>
- 
+        <button className="bg-rose-300 text-white font-semibold py-2 px-4 mt-4 rounded hover:bg-rose-00 ">
+          <Link href="/add-document">Get Started</Link>
+        </button>
+      </div>
     </div>
   );
 }
-
